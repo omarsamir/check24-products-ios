006dc380006fa3f55beaaffd339327c3ac910e35
Omar Samir <omar.mohamed2@vodafone.com>
2021-11-16 11:23:40 +0200

    init commit create project

---
+++ check24-products/check24-products/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  check24-products
+//
+//  Created by omar on 16/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
